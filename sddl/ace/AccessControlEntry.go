e0bcbcecf15990a42a086e9520b753e60d2876fe
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-05-24 18:57:54 +0200

    Created functions for AccessControlListRevision

--- sddl/ace/AccessControlEntry.go
+++ sddl/ace/AccessControlEntry.go
@@ -1,12 +1,5 @@
 package ace
 
-import (
-	"fmt"
-	"strings"
-
-	ntsd_ace "github.com/TheManticoreProject/winacl/ace"
-)
-
 // ParseSDDLAceString parses an SDDL ACE string into an NT Security Descriptor ACE.
 //
 // The SDDL ACE string format is: aceType;aceFlags;rights;objectGuid;inheritedObjectGuid;accountSid
@@ -17,32 +10,58 @@ import (
 //
 // Returns:
 //   - (*ntsd_ace.AccessControlEntry, error): The parsed ACE and any error that occurred
-func ParseSDDLAceString(aceString string) (*ntsd_ace.AccessControlEntry, error) {
-	// Split the ACE string into components
-	parts := strings.Split(aceString, ";")
-	if len(parts) < 6 {
-		return nil, fmt.Errorf("invalid ACE string format: %s", aceString)
-	}
+// func ParseSDDLAceString(aceString string) (*ntsd_ace.AccessControlEntry, error) {
+// 	// Split the ACE string into components
+// 	parts := strings.Split(aceString, ";")
+// 	if len(parts) < 6 {
+// 		return nil, fmt.Errorf("invalid ACE string format: %s", aceString)
+// 	}
 
-	aceType := parts[0]
-	aceFlags := parts[1]
-	rights := parts[2]
-	objectGuid := parts[3]
-	inheritedObjectGuid := parts[4]
-	accountSid := parts[5]
+// 	aceType := parts[0]
+// 	aceFlags := parts[1]
+// 	rights := parts[2]
+// 	objectGuid := parts[3]
+// 	inheritedObjectGuid := parts[4]
+// 	accountSid := parts[5]
 
-	// Create new ACE
-	ace := &ntsd_ace.AccessControlEntry{
-		Header: ntsd_ace.AccessControlEntryHeader{
-			Type: SDDLToACETypeMap[aceType],
-			// TODO: Parse ACE flags
-			// TODO: Calculate ACE size
-		},
-		// TODO: Parse rights into Mask
-		// TODO: Parse object GUID if present
-		// TODO: Parse inherited object GUID if present
-		// TODO: Parse account SID
-	}
+// 	// Create new ACE
+// 	ace := &ntsd_ace.AccessControlEntry{}
 
-	return ace, nil
-}
+// 	// Parse the ACE type
+// 	if _, ok := SDDLToACETypeMap[aceType]; ok {
+// 		ace.Header.Type.SetType(SDDLToACETypeMap[aceType])
+// 	} else {
+// 		return nil, fmt.Errorf("invalid ACE type: %s", aceType)
+// 	}
+
+// 	// Parse the ACE flags
+// 	if _, ok := SDDLToACEFlagsMap[aceFlags]; ok {
+// 		ace.Header.Flags.SetFlags(SDDLToACEFlagsMap[aceFlags])
+// 	} else {
+// 		return nil, fmt.Errorf("invalid ACE flags: %s", aceFlags)
+// 	}
+
+// 	// Parse the rights
+// 	if _, ok := SDDLToACERightsMap[rights]; ok {
+// 		ace.Mask.SetRights(SDDLToACERightsMap[rights])
+// 	} else {
+// 		return nil, fmt.Errorf("invalid ACE rights: %s", rights)
+// 	}
+
+// 	// Parse the object GUID
+// 	if objectGuid != "" {
+// 		ace.ObjectGuid.SetObjectGuid(objectGuid)
+// 	}
+
+// 	// Parse the inherited object GUID
+// 	if inheritedObjectGuid != "" {
+// 		ace.InheritedObjectGuid.SetObjectGuid(inheritedObjectGuid)
+// 	}
+
+// 	// Parse the account SID
+// 	if accountSid != "" {
+// 		ace.SID.SetSID(accountSid)
+// 	}
+
+// 	return ace, nil
+// }
